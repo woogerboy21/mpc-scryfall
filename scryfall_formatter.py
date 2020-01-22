d6fe70bac2e1bf54d31fc79c16cfabcdfdf78f9f
Nicholas de Paola <ndepaola97@gmail.com>
2020-01-22 11:22:39 +1000

    updated to suppport transform cards
    when you give it the name of a transform card's front or back, it'll process both faces for you

--- scryfall_formatter.py
+++ scryfall_formatter.py
@@ -27,138 +27,149 @@ def process_card(cardname, expansion=None):
         print("Couldn't find card: " + cardname)
         return
 
-    cardname = card["name"].replace("//", "&")  # should work on macOS & windows now
-    cardname = cardname.replace(":", "")  # case for Circle of Protection: X
+    # Handle cards with multiple faces
+    if card["layout"] == "transform":
+        # cardinfo = {x["name"]: x["image_uris"]["large"] for x in card["card_faces"]}
+        cards = [x for x in card["card_faces"]]
+    else:
+        # cardinfo = {card["name"]: card["image_uris"]["large"]}
+        cards = [card,]
 
-    # process with waifu2x
-    r = requests.post(
-        "https://api.deepai.org/api/waifu2x",
-        data={
-            'image': card["image_uris"]['large'],
-        },
-        headers={'api-key': config.TOKEN}
-    )
-    output_url = r.json()['output_url']
-    im = imageio.imread(output_url)
+    # for name, image_uri in cardinfo.items():
+    for card_obj in cards:
+        name = card_obj["name"].replace("//", "&")  # should work on macOS & windows now
+        name = name.replace(":", "")  # case for Circle of Protection: X
 
-    # Read in filter image
-    filterimage = np.copy(imageio.imread("./filterimagenew.png"))
+        # process with waifu2x
+        r = requests.post(
+            "https://api.deepai.org/api/waifu2x",
+            data={
+                'image': card_obj["image_uris"]["large"],
+            },
+            headers={'api-key': config.TOKEN}
+        )
+        output_url = r.json()['output_url']
+        im = imageio.imread(output_url)
 
-    # Resize filter to shape of input image
-    filterimage = resize(filterimage, [im.shape[0], im.shape[1]], anti_aliasing=True, mode="edge")
+        # Read in filter image
+        filterimage = np.copy(imageio.imread("./filterimagenew.png"))
 
-    # Initialise arrays
-    im_filtered = np.zeros(im.shape, dtype=np.complex_)
-    im_recon = np.zeros(im.shape, dtype=np.float_)
+        # Resize filter to shape of input image
+        filterimage = resize(filterimage, [im.shape[0], im.shape[1]], anti_aliasing=True, mode="edge")
 
-    # Apply filter to each RGB channel individually
-    for i in range(0, 3):
-        im_filtered[:, :, i] = np.multiply(fftshift(fft2(im[:, :, i])), filterimage)
-        im_recon[:, :, i] = ifft2(ifftshift(im_filtered[:, :, i])).real
+        # Initialise arrays
+        im_filtered = np.zeros(im.shape, dtype=np.complex_)
+        im_recon = np.zeros(im.shape, dtype=np.float_)
 
-    # Scale between 0 and 255 for uint8
-    minval = np.min(im_recon)
-    maxval = np.max(im_recon)
-    im_recon_sc = (255 * ((im_recon - minval) / (maxval - minval))).astype(np.uint8)
+        # Apply filter to each RGB channel individually
+        for i in range(0, 3):
+            im_filtered[:, :, i] = np.multiply(fftshift(fft2(im[:, :, i])), filterimage)
+            im_recon[:, :, i] = ifft2(ifftshift(im_filtered[:, :, i])).real
 
-    # TODO: pre-m15, post-8ed cards
-    # TODO: pre-8ed cards (?)
+        # Scale between 0 and 255 for uint8
+        minval = np.min(im_recon)
+        maxval = np.max(im_recon)
+        im_recon_sc = (255 * ((im_recon - minval) / (maxval - minval))).astype(np.uint8)
 
-    # Borderify image
-    pad = 57  # Pad image by 1/8th of inch on each edge
-    bordertol = 16  # Overfill onto existing border by 16px to remove white corners
-    im_padded = np.zeros([im.shape[0] + 2 * pad, im.shape[1] + 2 * pad, 3])
+        # TODO: pre-m15, post-8ed cards
+        # TODO: pre-8ed cards (?)
 
-    # Get border colour from left side of image
-    bordercolour = np.median(im_recon_sc[200:(im_recon_sc.shape[0] - 200), 0:bordertol], axis=(0, 1))
+        # Borderify image
+        pad = 57  # Pad image by 1/8th of inch on each edge
+        bordertol = 16  # Overfill onto existing border by 16px to remove white corners
+        im_padded = np.zeros([im.shape[0] + 2 * pad, im.shape[1] + 2 * pad, 3])
 
-    # Pad image
-    for i in range(0, 3):
-        im_padded[pad:im.shape[0] + pad, pad:im.shape[1] + pad, i] = im_recon_sc[:, :, i]
+        # Get border colour from left side of image
+        bordercolour = np.median(im_recon_sc[200:(im_recon_sc.shape[0] - 200), 0:bordertol], axis=(0, 1))
 
-    # Overfill onto existing border to remove white corners
-    # Left
-    im_padded[0:im_padded.shape[0],
-              0:pad + bordertol, :] = bordercolour
+        # Pad image
+        for i in range(0, 3):
+            im_padded[pad:im.shape[0] + pad, pad:im.shape[1] + pad, i] = im_recon_sc[:, :, i]
 
-    # Right
-    im_padded[0:im_padded.shape[0],
-              im_padded.shape[1] - (pad + bordertol):im_padded.shape[1], :] = bordercolour
+        # Overfill onto existing border to remove white corners
+        # Left
+        im_padded[0:im_padded.shape[0],
+                  0:pad + bordertol, :] = bordercolour
 
-    # Top
-    im_padded[0:pad + bordertol,
-              0:im_padded.shape[1], :] = bordercolour
+        # Right
+        im_padded[0:im_padded.shape[0],
+                  im_padded.shape[1] - (pad + bordertol):im_padded.shape[1], :] = bordercolour
 
-    # Bottom
-    im_padded[im_padded.shape[0] - (pad + bordertol):im_padded.shape[0],
-              0:im_padded.shape[1], :] = bordercolour
+        # Top
+        im_padded[0:pad + bordertol,
+                  0:im_padded.shape[1], :] = bordercolour
 
-    # Remove copyright line
-    # print(card["power"])
-    if card["frame"] == "2015":
-        # Modern frame
-        leftPix = 735
-        rightPix = 1140
-        topPix = 1550
-        bottomPix = 1585
+        # Bottom
+        im_padded[im_padded.shape[0] - (pad + bordertol):im_padded.shape[0],
+                  0:im_padded.shape[1], :] = bordercolour
 
-        # creatures have a shifted legal line
-        try:
-            power = card["power"]
-            toughness = card["toughness"]
-            topPix = 1575
-            bottomPix = 1615
-            # Creature card
-        except KeyError:
-            pass
+        # Remove copyright line
+        # print(card["power"])
+        if card["frame"] == "2015":
+            # Modern frame
+            leftPix = 735
+            rightPix = 1140
+            topPix = 1550
+            bottomPix = 1585
 
-        # planeswalkers have a shifted legal line too
-        try:
-            loyalty = card["loyalty"]
-            topPix = 1575
-            bottomPix = 1605
-        except KeyError:
-            pass
+            # creatures have a shifted legal line
+            try:
+                power = card_obj["power"]
+                toughness = card_obj["toughness"]
+                topPix = 1575
+                bottomPix = 1615
+                # Creature card
+            except KeyError:
+                pass
 
-        im_padded[topPix:bottomPix, leftPix:rightPix, :] = bordercolour
+            # planeswalkers have a shifted legal line too
+            try:
+                loyalty = card_obj["loyalty"]
+                topPix = 1575
+                bottomPix = 1615
+            except KeyError:
+                pass
 
-    elif card["frame"] == "2003":
-        # 8ED frame
-        try:
-            loyalty = card["loyalty"]
-            leftPix = 300
-            rightPix = 960
-            topPix = 1570
-            bottomPix = 1600
             im_padded[topPix:bottomPix, leftPix:rightPix, :] = bordercolour
-        except KeyError:
-            # TODO: Content aware fill?
-            pass
 
-    # Remove holostamp
-    if card["frame"] == "2015" and (card["rarity"] == "rare" or card["rarity"] == "mythic"):
-        # Need to remove holostamp
-        # Define bounds of ellipse to fill with border colour
-        leftE = 575
-        rightE = 690
-        topE = 1520
-        bottomE = 1575
+        elif card["frame"] == "2003":
+            # 8ED frame
+            try:
+                loyalty = card_obj["loyalty"]
+                leftPix = 300
+                rightPix = 960
+                topPix = 1570
+                bottomPix = 1600
+                im_padded[topPix:bottomPix, leftPix:rightPix, :] = bordercolour
+            except KeyError:
+                # TODO: Content aware fill?
+                pass
 
-        cx = (leftE + rightE) / 2
-        cy = (topE + bottomE) / 2
+        # Remove holostamp
+        if card["frame"] == "2015" and (card["rarity"] == "rare" or card["rarity"] == "mythic") \
+                and "/large/front/" in card_obj["image_uris"]["large"]:
+            # Need to remove holostamp
+            # Define bounds of ellipse to fill with border colour
+            leftE = 575
+            rightE = 690
+            topE = 1520
+            bottomE = 1575
 
-        h = (bottomE - topE) / 2
-        w = (rightE - leftE) / 2
+            cx = (leftE + rightE) / 2
+            cy = (topE + bottomE) / 2
 
-        for x in range(leftE, rightE + 1):
-            for y in range(topE, bottomE + 1):
-                # determine if point is in the holostamp area
-                if pow(x - cx, 2) / pow(w, 2) + pow(y - cy, 2) / pow(h, 2) <= 1:
-                    # point is inside ellipse
-                    im_padded[y, x, :] = bordercolour
+            h = (bottomE - topE) / 2
+            w = (rightE - leftE) / 2
 
-    # Write image to disk
-    imageio.imwrite("formatted/" + cardname + ".png", im_padded.astype(np.uint8))
+            for x in range(leftE, rightE + 1):
+                for y in range(topE, bottomE + 1):
+                    # determine if point is in the holostamp area
+                    if pow(x - cx, 2) / pow(w, 2) + pow(y - cy, 2) / pow(h, 2) <= 1:
+                        # point is inside ellipse
+                        im_padded[y, x, :] = bordercolour
+
+        # Write image to disk
+        imageio.imwrite("formatted/" + name + ".png", im_padded.astype(np.uint8))
 
 
 if __name__ == "__main__":
